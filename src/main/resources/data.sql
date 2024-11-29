33bc37362538329b3f73bf118003a58bbb17b69d
khuong2924 <truongkhuong666@gmail.com>
2024-11-29 15:48:33 +0700

    add datasql

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -1,22 +1,287 @@
-use midtermjava;
+CREATE DATABASE  IF NOT EXISTS `midtermjava` /*!40100 DEFAULT CHARACTER SET utf8mb4 COLLATE utf8mb4_0900_ai_ci */ /*!80016 DEFAULT ENCRYPTION='N' */;
+USE `midtermjava`;
+-- MySQL dump 10.13  Distrib 8.0.40, for Win64 (x86_64)
+--
+-- Host: 127.0.0.1    Database: midtermjava
+-- ------------------------------------------------------
+-- Server version	8.4.0
 
-INSERT INTO user VALUES ('admin@gmail.com', 'My', 'Admin', 123)
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
-INSERT INTO category(name) VALUES
-                               ('Laptops'),
-                               ('Smartphones'),
-                               ('Tablets'),
-                               ('Accessories'),
-                               ('Software');
+--
+-- Table structure for table `cart`
+--
 
-INSERT INTO product(name, category_id, price, weight, description, image_product, brand, color) VALUES
+DROP TABLE IF EXISTS `cart`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart` (
+                        `id` bigint NOT NULL AUTO_INCREMENT,
+                        `user_id` bigint DEFAULT NULL,
+                        PRIMARY KEY (`id`),
+                        UNIQUE KEY `UK9emlp6m95v5er2bcqkjsw48he` (`user_id`),
+                        CONSTRAINT `FKg5uhi8vpsuy0lgloxk2h4w5o6` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
 
-                                                                                                    ('LG UltraWide Monitor 34-inch', 3, 799, 5.0, 'A 34-inch ultra-wide monitor with 144Hz refresh rate, perfect for multitasking and gaming.', 'https://res.cloudinary.com/dvzhmi7a9/image/upload/v1727329427/ECommerce/lg-ultrawide.png', 'LG', 'Black'),
-                                                                                                    ('Bose QuietComfort 35 II', 5, 299, 0.24, 'Noise-cancelling headphones with superior sound quality, comfortable fit, and long battery life.', 'https://res.cloudinary.com/dvzhmi7a9/image/upload/v1727329424/ECommerce/bose-headphones.png', 'Dell', 'White'),
-                                                                                                    ('Apple Watch Series 8', 4, 399, 0.03, 'Smartwatch with advanced health features, fitness tracking, and cellular connectivity.', 'https://res.cloudinary.com/dvzhmi7a9/image/upload/v1727329426/ECommerce/apple-watch.png', 'MSI', 'Blue'),
-                                                                                                    ('Razer DeathAdder V2', 4, 69, 0.08, 'High-precision gaming mouse with 20,000 DPI, Razer Optical Mouse Switches, and ergonomic design.', 'https://res.cloudinary.com/dvzhmi7a9/image/upload/v1727329425/ECommerce/razer-mouse.png', 'Logitech', 'Black'),
-                                                                                                    ('Google Drive 1TB', 5, 99, 0.0, 'Cloud storage plan offering 1TB of space, perfect for storing and sharing files with ease.', 'https://res.cloudinary.com/dvzhmi7a9/image/upload/v1727329423/ECommerce/google-drive.png', 'Google', 'Green'),
-                                                                                                    ('Seagate Backup Plus 5TB', 5, 129, 0.6, 'Portable external hard drive with 5TB capacity, perfect for backing up your important data.', 'https://res.cloudinary.com/dvzhmi7a9/image/upload/v1727329427/ECommerce/seagate-drive.png', 'Microsoft', 'White'),
-                                                                                                    ('Sony WH-1000XM5', 1, 349, 0.25, 'Premium noise-cancelling headphones with exceptional sound, comfort, and up to 30 hours of battery life.', 'https://res.cloudinary.com/dvzhmi7a9/image/upload/v1727329426/ECommerce/sony-headphones.png', 'Sony', 'Black'),
-                                                                                                    ('Samsung Galaxy Watch 5', 2, 279, 0.04, 'Smartwatch with health monitoring features, a sleek design, and long-lasting battery life.', 'https://res.cloudinary.com/dvzhmi7a9/image/upload/v1727329424/ECommerce/samsung-watch.png', 'SamSung', 'Black'),
-                                                                                                    ('Logitech G Pro X Wireless', 1, 149, 0.1, 'Wireless gaming headset with Blue VO!CE technology, pro-level sound, and comfort for long gaming sessions.', 'https://res.cloudinary.com/dvzhmi7a9/image/upload/v1727329425/ECommerce/logitech-headset.png', 'Logitech', 'White');
+--
+-- Dumping data for table `cart`
+--
+
+LOCK TABLES `cart` WRITE;
+/*!40000 ALTER TABLE `cart` DISABLE KEYS */;
+INSERT INTO `cart` VALUES (1,1),(2,2),(3,3);
+/*!40000 ALTER TABLE `cart` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `cart_item`
+--
+
+DROP TABLE IF EXISTS `cart_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `cart_item` (
+                             `id` bigint NOT NULL AUTO_INCREMENT,
+                             `quantity` int DEFAULT NULL,
+                             `cart_id` bigint DEFAULT NULL,
+                             `product_id` bigint DEFAULT NULL,
+                             PRIMARY KEY (`id`),
+                             KEY `FK1uobyhgl1wvgt1jpccia8xxs3` (`cart_id`),
+                             KEY `FKjcyd5wv4igqnw413rgxbfu4nv` (`product_id`),
+                             CONSTRAINT `FK1uobyhgl1wvgt1jpccia8xxs3` FOREIGN KEY (`cart_id`) REFERENCES `cart` (`id`),
+                             CONSTRAINT `FKjcyd5wv4igqnw413rgxbfu4nv` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `cart_item`
+--
+
+LOCK TABLES `cart_item` WRITE;
+/*!40000 ALTER TABLE `cart_item` DISABLE KEYS */;
+INSERT INTO `cart_item` VALUES (18,1,2,1),(19,1,2,2),(21,1,NULL,1),(22,1,NULL,2),(23,1,1,4),(24,1,1,1);
+/*!40000 ALTER TABLE `cart_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `category`
+--
+
+DROP TABLE IF EXISTS `category`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `category` (
+                            `category_id` bigint NOT NULL AUTO_INCREMENT,
+                            `name` varchar(255) DEFAULT NULL,
+                            PRIMARY KEY (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=6 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `category`
+--
+
+LOCK TABLES `category` WRITE;
+/*!40000 ALTER TABLE `category` DISABLE KEYS */;
+INSERT INTO `category` VALUES (1,'Laptops'),(2,'Smartphones'),(3,'Tablets'),(4,'Accessories'),(5,'Others');
+/*!40000 ALTER TABLE `category` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `order_item`
+--
+
+DROP TABLE IF EXISTS `order_item`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `order_item` (
+                              `id` bigint NOT NULL AUTO_INCREMENT,
+                              `quantity` int DEFAULT NULL,
+                              `order_id` bigint DEFAULT NULL,
+                              `product_id` bigint DEFAULT NULL,
+                              PRIMARY KEY (`id`),
+                              KEY `FKt4dc2r9nbvbujrljv3e23iibt` (`order_id`),
+                              KEY `FK551losx9j75ss5d6bfsqvijna` (`product_id`),
+                              CONSTRAINT `FK551losx9j75ss5d6bfsqvijna` FOREIGN KEY (`product_id`) REFERENCES `product` (`id`),
+                              CONSTRAINT `FKt4dc2r9nbvbujrljv3e23iibt` FOREIGN KEY (`order_id`) REFERENCES `orders` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `order_item`
+--
+
+LOCK TABLES `order_item` WRITE;
+/*!40000 ALTER TABLE `order_item` DISABLE KEYS */;
+/*!40000 ALTER TABLE `order_item` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `orders`
+--
+
+DROP TABLE IF EXISTS `orders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orders` (
+                          `id` bigint NOT NULL AUTO_INCREMENT,
+                          `delivery_address` varchar(255) DEFAULT NULL,
+                          `order_date` datetime(6) DEFAULT NULL,
+                          `status` varchar(255) DEFAULT NULL,
+                          `user_id` bigint DEFAULT NULL,
+                          PRIMARY KEY (`id`),
+                          KEY `FK32ql8ubntj5uh44ph9659tiih` (`user_id`),
+                          CONSTRAINT `FK32ql8ubntj5uh44ph9659tiih` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orders`
+--
+
+LOCK TABLES `orders` WRITE;
+/*!40000 ALTER TABLE `orders` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orders` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `product`
+--
+
+DROP TABLE IF EXISTS `product`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `product` (
+                           `id` bigint NOT NULL AUTO_INCREMENT,
+                           `brand` varchar(255) DEFAULT NULL,
+                           `color` varchar(255) DEFAULT NULL,
+                           `description` varchar(255) DEFAULT NULL,
+                           `image_product` varchar(255) DEFAULT NULL,
+                           `name` varchar(255) DEFAULT NULL,
+                           `price` double NOT NULL,
+                           `weight` double NOT NULL,
+                           `category_id` bigint DEFAULT NULL,
+                           PRIMARY KEY (`id`),
+                           KEY `FK1mtsbur82frn64de7balymq9s` (`category_id`),
+                           CONSTRAINT `FK1mtsbur82frn64de7balymq9s` FOREIGN KEY (`category_id`) REFERENCES `category` (`category_id`)
+) ENGINE=InnoDB AUTO_INCREMENT=10 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `product`
+--
+
+LOCK TABLES `product` WRITE;
+/*!40000 ALTER TABLE `product` DISABLE KEYS */;
+INSERT INTO `product` VALUES (1,'LG','Black','A 34-inch ultra-wide monitor with 144Hz refresh rate, perfect for multitasking and gaming.','http://res.cloudinary.com/dhp7ylyvh/image/upload/v1732452954/anzzpetyu0pmhbka74pd.jpg','LG UltraWide Monitor 34-inch',799,5,3),(2,'Dell','White','Noise-cancelling headphones with superior sound quality, comfortable fit, and long battery life.','http://res.cloudinary.com/dhp7ylyvh/image/upload/v1732453018/outouublrj13l24rkjv3.webp','Bose QuietComfort 35 II',299,0.24,5),(3,'MSI','Blue','Smartwatch with advanced health features, fitness tracking, and cellular connectivity.','http://res.cloudinary.com/dhp7ylyvh/image/upload/v1732453044/c5ca7ssc9bug7ivjan5v.jpg','Apple Watch Series 8',399,0.03,4),(4,'Logitech','Black','High-precision gaming mouse with 20,000 DPI, Razer Optical Mouse Switches, and ergonomic design.','http://res.cloudinary.com/dhp7ylyvh/image/upload/v1732453060/s4pm7nqaxpdbau6axh5j.jpg','Razer DeathAdder V2',69,0.08,4),(5,'Google','Green','Cloud storage plan offering 1TB of space, perfect for storing and sharing files with ease.','http://res.cloudinary.com/dhp7ylyvh/image/upload/v1732453073/ds1ts7mf1rm3cntjjaid.png','Google Drive 1TB',99,0,5),(6,'Microsoft','White','Portable external hard drive with 5TB capacity, perfect for backing up your important data.','http://res.cloudinary.com/dhp7ylyvh/image/upload/v1732453088/puuzfsz90fkjnzoxe4jc.jpg','Seagate Backup Plus 5TB',129,0.6,5),(7,'Sony','Black','Premium noise-cancelling headphones with exceptional sound, comfort, and up to 30 hours of battery life.','http://res.cloudinary.com/dhp7ylyvh/image/upload/v1732453104/ihlheu43kvjhykvjkph5.png','Sony WH-1000XM5',349,0.25,1),(8,'SamSung','Black','Smartwatch with health monitoring features, a sleek design, and long-lasting battery life.','http://res.cloudinary.com/dhp7ylyvh/image/upload/v1732453121/ub8u0jqlinrbwtqhmi3v.jpg','Samsung Galaxy Watch 5',279,0.04,2),(9,'Logitech','White','Wireless gaming headset with Blue VO!CE technology, pro-level sound, and comfort for long gaming sessions.','http://res.cloudinary.com/dhp7ylyvh/image/upload/v1732453138/xm6sflmzjomzionz9lqh.jpg','Logitech G Pro X Wireless',149,0.1,1);
+/*!40000 ALTER TABLE `product` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `roles`
+--
+
+DROP TABLE IF EXISTS `roles`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `roles` (
+                         `id` int NOT NULL AUTO_INCREMENT,
+                         `name` varchar(255) NOT NULL,
+                         PRIMARY KEY (`id`),
+                         UNIQUE KEY `UKofx66keruapi6vyqpv6f2or37` (`name`)
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `roles`
+--
+
+LOCK TABLES `roles` WRITE;
+/*!40000 ALTER TABLE `roles` DISABLE KEYS */;
+INSERT INTO `roles` VALUES (1,'ROLE_ADMIN'),(2,'ROLE_USER');
+/*!40000 ALTER TABLE `roles` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `user_role`
+--
+
+DROP TABLE IF EXISTS `user_role`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `user_role` (
+                             `user_id` bigint NOT NULL,
+                             `role_id` int NOT NULL,
+                             KEY `FKt7e7djp752sqn6w22i6ocqy6q` (`role_id`),
+                             KEY `FKj345gk1bovqvfame88rcx7yyx` (`user_id`),
+                             CONSTRAINT `FKj345gk1bovqvfame88rcx7yyx` FOREIGN KEY (`user_id`) REFERENCES `users` (`id`),
+                             CONSTRAINT `FKt7e7djp752sqn6w22i6ocqy6q` FOREIGN KEY (`role_id`) REFERENCES `roles` (`id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `user_role`
+--
+
+LOCK TABLES `user_role` WRITE;
+/*!40000 ALTER TABLE `user_role` DISABLE KEYS */;
+INSERT INTO `user_role` VALUES (1,1),(1,2);
+/*!40000 ALTER TABLE `user_role` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `users`
+--
+
+DROP TABLE IF EXISTS `users`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `users` (
+                         `id` bigint NOT NULL AUTO_INCREMENT,
+                         `email` varchar(255) NOT NULL,
+                         `first_name` varchar(255) NOT NULL,
+                         `last_name` varchar(255) DEFAULT NULL,
+                         `password` varchar(255) DEFAULT NULL,
+                         PRIMARY KEY (`id`),
+                         UNIQUE KEY `UK6dotkott2kjsp8vw4d0m25fb7` (`email`)
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_0900_ai_ci;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `users`
+--
+
+LOCK TABLES `users` WRITE;
+/*!40000 ALTER TABLE `users` DISABLE KEYS */;
+INSERT INTO `users` VALUES (1,'admin@gmail.com','Admin',NULL,'123'),(2,'kkk@gmail.com','Truong','Khuong','123'),(3,'kkkk@gmail.com','Truong','Khuong','123');
+/*!40000 ALTER TABLE `users` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Dumping events for database 'midtermjava'
+--
+
+--
+-- Dumping routines for database 'midtermjava'
+--
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2024-11-29 15:33:59
